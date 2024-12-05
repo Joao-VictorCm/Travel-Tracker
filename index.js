55b1749fa4c885f3b72ab32e7a90348abf1bb4a8
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-04 22:05:49 -0300

    Finalizado rota post que colore o pais já visitado digitado pelo usuario

--- index.js
+++ index.js
@@ -34,6 +34,27 @@ console.log(result.rows);
 res.render("index.ejs", {countries: countries, total: countries.length})
 });
 
+app.post("/add", async (req, res)=>{
+  const input = req.body["country"] //Retorno do input digitado pelo usuario
+  console.log(input)
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",  // seleciona o country_code da tabele countries
+    [input]                                                   // com a comparação WHERE selecio o codigo do pais digitado pelo usuario
+  )
+  
+  if(result.rows.length !== 0){ //Verifica se o nome digitado pelo usuario não é igual a zero ou seja se tem no banco de dados 
+    const data = result.rows[0]
+    const countryCode = data.country_code //colocando o resultado em uma constante 
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", //Inserindo na tabela visited_countries na coluna country_code o valor retornado no countryCode
+      [countryCode]
+    )
+    res.redirect("/")
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
